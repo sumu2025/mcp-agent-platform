f9086841652756d9284c636a5dbed5bf8bab27f2
sumu2025 <weipeacock@gmail.com>
2025-04-04 20:24:59 +0800

    Initial DeepSeek API integration module

---
+++ src/integrations/deepseek_api.py
@@ -0,0 +1,21 @@
+class DeepSeekAPIIntegration:
+    """
+    DeepSeek API集成模块
+    提供API调用、token管理等基础功能
+    """
+    def __init__(self, api_key):
+        self.api_key = api_key
+    
+    def generate_text(self, prompt):
+        """
+        基础文本生成方法
+        """
+        # TODO: 实现实际的API调用逻辑
+        pass
+
+    def manage_tokens(self):
+        """
+        Token管理方法
+        """
+        # TODO: 实现Token使用追踪
+        pass
